c42672944fc4e087d2a48f11ffab550811f8bc12
Vikash giri <vikashgiri9027@gmail.com>
2025-09-21 05:51:40 +0530

    Add files via upload

--- config.php
+++ config.php
@@ -1,14 +1,14 @@
 <?php
-$host = "localhost";
-$user = "root";
-$password = "";
-$database = "users_db";
+
+$host = "sql313.infinityfree.com";
+$user = "if0_39757934";
+$password = "RaJamao5cCrk";
+$database = "if0_39757934_vikash_hms";
 
 $conn = new mysqli($host,$user,$password,$database);
 
 if($conn->connect_error){
     die("Connection failed:".$conn->connect_error);
 }
-
 
 ?>
